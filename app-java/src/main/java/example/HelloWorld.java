c2fb390635211e033b5fdbed0b9e8ea55ac95223
ghanigreen <ghanigreentech@gmail.com>
2018-02-04 09:42:15 +0530

    Update HelloWorld.java

--- app-java/src/main/java/example/HelloWorld.java
+++ app-java/src/main/java/example/HelloWorld.java
@@ -1,10 +1,11 @@
 package example;
 
 public class HelloWorld {
-
+int abc
+  //username =something password =12345667
   public void coveredByUnitTest() {
     System.out.println("coveredByUnitTest1");
-    System.out.println("coveredByUnitTest2);
+    System.out.println("coveredByUnitTest2");
   }
 
   public void coveredByIntegrationTest() {
