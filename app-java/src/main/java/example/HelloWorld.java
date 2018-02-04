802d9fc83ffb2aeeca9325d43a4de064b8c4f044
ghanigreen <ghanigreentech@gmail.com>
2018-02-04 09:39:49 +0530

    Update HelloWorld.java

--- app-java/src/main/java/example/HelloWorld.java
+++ app-java/src/main/java/example/HelloWorld.java
@@ -4,7 +4,7 @@ public class HelloWorld {
 
   public void coveredByUnitTest() {
     System.out.println("coveredByUnitTest1");
-    System.out.println("coveredByUnitTest2");
+    System.out.println("coveredByUnitTest2);
   }
 
   public void coveredByIntegrationTest() {
